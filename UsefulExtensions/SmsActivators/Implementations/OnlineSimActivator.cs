21e085d20ec8b18adb664dd41f766ce853c35d94
anden-t <dentolm6@gmail.com>
2021-08-22 00:20:50 +0300

    Adding OnlineSIm

---
+++ UsefulExtensions/SmsActivators/Implementations/OnlineSimActivator.cs
@@ -0,0 +1,8 @@
+﻿namespace UsefulExtensions.SmsActivators.Implementations
+{
+    public class OnlineSimActivator : SmsActivatorApiBase
+    {
+        protected override string ApiUrl => "http://api-conserver.onlinesim.ru/stubs/handler_api.php";
+        public OnlineSimActivator(string apiKey) : base(apiKey) { }
+    }
+}
