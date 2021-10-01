ffa6075a865922bc03c1f82fc53765618a4b0554
anden-t <dentolm6@gmail.com>
2021-10-01 17:01:09 +0300

    Adding CapmonsterCloudSolver

---
+++ UsefulExtensions/CaptchaSolvers/Implementations/CapmonsterCloudSolver.cs
@@ -0,0 +1,93 @@
+﻿using Leaf.xNet;
+using Newtonsoft.Json;
+using System.Threading;
+using UsefulExtensions.CaptchaSolvers.Exceptions;
+
+namespace UsefulExtensions.CaptchaSolvers.Implementations
+{
+    /// <summary>
+    /// Реализация интерфейса <see cref="ICaptchaSolver"/> для сервиса CapmonsterCloud <c>(https://capmonster.cloud/)</c>
+    /// </summary>
+    public class CapmonsterCloudSolver : ICaptchaSolver
+    {
+        private const string CAPTCHA_NOT_READY = "CAPCHA_NOT_READY";
+
+        public event OnLogMessageHandler OnLogMessage;
+
+        public ProxyClient Proxy { get; set; }
+
+        public string Key { get; }
+
+        public CapmonsterCloudSolver(string key)
+        {
+            Key = key;
+        }
+
+        public string SolveArkoseCaptcha(string publicKey, string surl, string pageUrl)
+        {
+            string inUrl = $"https://api.capmonster.cloud/in.php?" +
+                           $"key={Key}" +
+                           $"&method=funcaptcha" +
+                           $"&publickey={publicKey}" +
+                           $"&surl={surl}" +
+                           $"&pageurl={pageUrl}" +
+                           $"&json=1";
+
+            return GetAnswer(inUrl);
+        }
+
+        private string GetAnswer(string inUrl)
+        {
+            HttpRequest request = new HttpRequest();
+            if (Proxy != null)
+                request.Proxy = Proxy;
+
+            string inResponseString = request.Get(inUrl).ToString();
+            RucaptchaResponse inResponse = JsonConvert.DeserializeObject<RucaptchaResponse>(inResponseString);
+
+            if (inResponse.Status != 1)
+                throw new InvalidRequestException(inResponse.Request);
+
+            OnLogMessage?.Invoke(this, new OnLogMessageEventArgs($"Captcha sended | ID = {inResponse.Request}"));
+
+            RucaptchaResponse solveResponse = new RucaptchaResponse() { Status = 0, Request = CAPTCHA_NOT_READY };
+
+            while (solveResponse.Status == 0)
+            {
+                Thread.Sleep(5000);
+                string solveResponseString = request.Get($"https://api.capmonster.cloud/res.php?key={Key}&action=get&json=1&id={inResponse.Request}").ToString();
+                solveResponse = JsonConvert.DeserializeObject<RucaptchaResponse>(solveResponseString);
+                if (solveResponse.Status == 0 && solveResponse.Request != CAPTCHA_NOT_READY)
+                    throw new InvalidRequestException(solveResponse.Request);
+                OnLogMessage?.Invoke(this, new OnLogMessageEventArgs($"Captcha result: {solveResponse.Request}"));
+            }
+
+            return solveResponse.Request;
+        }
+
+        public string SolveRecaptchaV2(string siteKey, string pageUrl, bool invisible)
+        {
+            string inUrl = $"https://api.capmonster.cloud/in.php?" +
+                           $"key={Key}" +
+                           $"&method=userrecaptcha" +
+                           $"&googlekey={siteKey}" +
+                           $"&pageurl={pageUrl}" +
+                           $"&invisible={(invisible ? "1" : "0")}" +
+                           $"&json=1";
+
+            return GetAnswer(inUrl);
+        }
+
+        public string SolveHCaptcha(string siteKey, string pageUrl)
+        {
+            string inUrl = $"https://api.capmonster.cloud/in.php?" +
+                           $"key={Key}" +
+                           $"&method=hcaptcha" +
+                           $"&sitekey={siteKey}" +
+                           $"&pageurl={pageUrl}" +
+                           $"&json=1";
+
+            return GetAnswer(inUrl);
+        }
+    }
+}
