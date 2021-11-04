ea7f9194f8289c9c7fd7528b64a1ec397ffbd8a7
anden-t <dentolm6@gmail.com>
2021-11-05 01:35:41 +0300

    Imap support

---
+++ UsefulExtensions/Mail/IImapServerSelector.cs
@@ -0,0 +1,15 @@
+﻿namespace UsefulExtensions.Mail
+{
+    /// <summary>
+    /// Интерфейс для получения imap сервера по домену эл. почты
+    /// </summary>
+    public interface IImapServerSelector
+    {
+        /// <summary>
+        /// Получает сервер для заданного домена эл. почты
+        /// </summary>
+        /// <param name="domain">Домен эл. почты</param>
+        /// <returns></returns>
+        ImapServer GetServer(string domain);
+    }
+}
---
+++ UsefulExtensions/Mail/ImapServerSelector.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace UsefulExtensions.Mail
+{
+    /// <summary>
+    /// Абстрактный класс для соотношения доменов эл. почты и IMAP серверов
+    /// </summary>
+    public abstract class ImapServerSelector : IImapServerSelector
+    {
+        /// <summary>
+        /// Словарь домен - сервер
+        /// </summary>
+        public Dictionary<string, ImapServer> Servers { get; protected set; }
+
+        /// <summary>
+        /// Конструктор по умолчанию
+        /// </summary>
+        public ImapServerSelector()
+        {
+            Servers = new Dictionary<string, ImapServer>();
+        }
+
+        /// <summary>
+        /// Получает сервер для заданного домена эл. почты
+        /// </summary>
+        /// <param name="domain">Домен эл. почты</param>
+        /// <returns></returns>
+        public ImapServer GetServer(string domain)
+        {
+            foreach (var server in Servers)
+            {
+                if (domain.ToLower() == server.Key.ToLower())
+                    return server.Value;
+            }
+
+            return null;
+        }
+    }
+}
