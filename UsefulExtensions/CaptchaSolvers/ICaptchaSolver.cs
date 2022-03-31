43bc43bbf7e0f375d34f461d5040f88505c6e3b4
maass78 <anden.tolm@gmail.com>
2022-03-31 22:45:13 +0300

    v1.1.0
    выпилил imap, сделаю в другой либе

--- UsefulExtensions/CaptchaSolvers/ICaptchaSolver.cs
+++ UsefulExtensions/CaptchaSolvers/ICaptchaSolver.cs
@@ -46,9 +46,6 @@ namespace UsefulExtensions.CaptchaSolvers
         /// <returns>Токен решенной капчи</returns>
         string SolveHCaptcha(string siteKey, string pageUrl, bool invisible = false, string additionalData = null);
 
-
-        string SolvePictureCaptcha(string )
-
         /// <summary>
         /// Прокси
         /// </summary>
