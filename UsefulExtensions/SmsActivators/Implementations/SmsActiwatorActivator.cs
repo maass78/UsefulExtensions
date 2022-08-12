0463eaf062fddda23b962257c082c28e92073b04
anden-t <80489596+anden-t@users.noreply.github.com>
2022-08-12 19:39:50 +0300

    Добавил sms-acktiwator.ru и sim5.net

---
+++ UsefulExtensions/SmsActivators/Implementations/SmsActiwatorActivator.cs
@@ -0,0 +1,9 @@
+﻿namespace UsefulExtensions.SmsActivators.Implementations
+{
+    internal class SmsActiwatorActivator : SmsActivatorApiBase
+    {
+        protected override string ApiUrl => "https://sms-acktiwator.ru/stubs/handler_api.php";
+
+        public SmsActiwatorActivator(string apiKey) : base(apiKey) { }
+    }
+}
